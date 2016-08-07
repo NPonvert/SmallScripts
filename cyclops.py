dbf20bd3ca5aa605682e313403ea55fc19dd7a6a
NPonvert <nathaniel_ponvert@brown.edu>
2016-08-07 09:48:37 -0400

    Update and rename dyklops.py to cyclops.py

--- cyclops.py
+++ cyclops.py
@@ -1,6 +1,6 @@
 import re
 from Bio import Entrez
-#Entrez.email = "" 
+Entrez.email = "nathaniel_ponvert@brown.edu" 
 infile = raw_input('infilename: ')
 outfile = raw_input('outfilename: ')
 outfile1 = raw_input('file for Athaliana only results: ')
@@ -9,15 +9,18 @@ outdict = {}
 with open(infile) as file:
 	for line in file:
 		if '#' not in line:
-			HitID = re.findall('^gi\|\d*\|\w*\|.{5,20}?\|\d*\|\w*\|(.{5,20}?)\|',line)
+			HitID = re.findall('^.{10,30}\tgi\|\d*\|\w*\|(.{5,20}?)\|',line)
 			Ident = re.findall('.*\t(\d*[.]{1}[\d]{3})', line)
 			if HitID:
 				outlist = []
 				VAR = str(HitID).replace('[','').replace(']','').replace("'","")
-				handle = Entrez.efetch(db="protein", id=VAR, rettype="gb", retmode="text")	
+				try:
+					handle = Entrez.efetch(db="protein", id=VAR, rettype="gb", retmode="text")	
+				except Exception:
+					pass
 				for line in handle:
-					species = re.findall('\[([.\w\s]*)\]', line, re.DOTALL)
-					atnum = re.findall('/[lg][oe][cn][ue][s]?[\_]?[t]?[a]?[g]?=".*\_*([Aa][RrTt][Aa\d][LlGg][Yy]?[Dd\d]{0,5}[Rr]?[Aa]?[Ff]?[Tt]?\_?[\d]{0,10})"', line)
+					species = re.findall('"taxon:([\d]+)"', line)
+					atnum = re.findall('\/[lg][oe][cn][ue][s]?[\_]?[t]?[a]?[g]?=".*\_*([Aa][RrTt][Aa\d][LlGg][Yy]?[Dd\d]{0,5}[Rr]?[Aa]?[Ff]?[Tt]?\_?[\d]{0,10})"', line)
 					eco = re.findall('/ecotype="(.?)\s*.*"', line)
 					if atnum:
 						if atnum not in outlist:
@@ -27,10 +30,14 @@ with open(infile) as file:
 					if eco:	
 						outlist.append(eco)
 					outlist = sorted(outlist)
-				outlist.append(HitID), outlist.append(Ident)
+				outlist.append(HitID)
+				outlist.append(Ident)
 				key = str(outlist).replace('[','').replace(']','').replace('(','').replace(')','').replace('\'','').replace(', ','_').replace(' ','_')
 				print key
-				l1 = Entrez.efetch(db="protein", id=VAR, rettype="fasta", retmode="text")
+				try:
+					l1 = Entrez.efetch(db="protein", id=VAR, rettype="fasta", retmode="text")
+				except Exception:
+					pass
 				for line in l1:
 					if '>' in line:
 						outdict[key] = ''
@@ -43,7 +50,7 @@ with open(infile) as file:
 			file.write('{}\n'.format(outdict[i]))
 	with open(outfile1, 'w') as file:
 		for i in outdict.keys():
-			if 'thaliana' in i:
+			if '3702' in i:
 				file.write('\n>{}\n'.format(i))
 				file.write('{}\n'.format(outdict[i]))
 	with open(outfile2, 'a') as file:
